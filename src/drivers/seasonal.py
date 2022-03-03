e6cfec691b417a64e46e1f81e655926a9a0f7c19
kjhall <kjhall@iri.columbia.edu>
2022-03-03 14:33:32 -0500

    changed behavior of seasonal driver

--- src/drivers/seasonal.py
+++ src/drivers/seasonal.py
@@ -50,17 +50,15 @@ class SeasonalDriver(intake.source.base.DataSource):
         first_fcst = pd.Timestamp( self.catalog_object.describe()['metadata']['hindcast_limits']['start'] ) # cftime.num2date(self.catalog_object.describe()['metadata']['hindcast_limits']['start'], self.catalog_object.describe()['metadata']['hindcast_limits']['units'], calendar=self.catalog_object.describe()['metadata']['hindcast_limits']['calendar'])
         last_fcst = pd.Timestamp( self.catalog_object.describe()['metadata']['hindcast_limits']['end'] ) if type( self.catalog_object.describe()['metadata']['hindcast_limits']['end'] ) == str else pd.Timestamp.today() # cftime.num2date(self.catalog_object.describe()['metadata']['hindcast_limits']['end'], self.catalog_object.describe()['metadata']['hindcast_limits']['units'], calendar=self.catalog_object.describe()['metadata']['hindcast_limits']['calendar']) if self.catalog_object.describe()['metadata']['hindcast_limits']['end'] != -1 else fdate
         
-        final_year = min(pd.Timestamp(final_year, fdate.month, 1), last_fcst).year if final_year is not None else last_fcst.year
-        first_year = max(pd.Timestamp(first_year, fdate.month, 1), first_fcst).year if first_year is not None else first_fcst.year
-
         if first_year is not None:
             assert first_year <= last_fcst.year, 'first_year ({}) must be earlier than or equal to the last available year ({})'.format(first_year, last_fcst.year)
-            if first_year < first_fcst.year:
-                print('first year "{}" not in range for {}: ({}-{}). Using first possible year!'.format(first_year, self.catalog_object.configure_new().name, first_fcst.year, last_fcst.year ))
+        else:
+            first_year = first_fcst.year
+       
         if final_year is not None:
             assert final_year >= first_fcst.year, 'final_year ({}) must be later than or equal to the first available year ({})'.format(final_year, first_fcst.year)
-            if final_year >= last_fcst.year:
-                print('final year "{}" not in range for {}: ({}-{}). Using last possible year!'.format(final_year, self.catalog_object.configure_new().name, last_fcst.year, first_fcst.year ))
+        else:
+            final_year = last_fcst.year
 
 
         fdate, target, lead_low, lead_high = seasonal_target(fdate, target, lead_low, lead_high)
@@ -68,7 +66,7 @@ class SeasonalDriver(intake.source.base.DataSource):
         use_dlauth = str(self.catalog_object.describe()['metadata']['dlauth_required'])
         return download(url, destination, verbose=verbose, format=filetype, use_dlauth=use_dlauth)
 
-    def forecasts(self, predictor_extent, fdate=pd.Timestamp.now() - pd.Timedelta(days=16), target=None, lead_low=None, lead_high=None, first_year=None, final_year=None, pressure=None, destination=None, filetype='cptv10.tsv', verbose=True):
+    def forecasts(self, predictor_extent, fdate=pd.Timestamp.now() - pd.Timedelta(days=16), target=None, lead_low=None, lead_high=None,  pressure=None, destination=None, filetype='cptv10.tsv', verbose=True):
         assert filetype in ['data.nc', 'cptv10.tsv'], 'invalid format {}'.format(filetype)
         assert fdate <= pd.Timestamp.now(), "Cannot make a forecast for a future date"
         assert target is not None or (lead_low is not None and lead_high is not None), "You must either supply a target season, or high and low lead-time coordinates, or a set of all three that agree"
@@ -84,24 +82,12 @@ class SeasonalDriver(intake.source.base.DataSource):
             assert True, 'The slow bird gets the worm'
             
         destination = destination +'.'+ filetype.split('.')[1]
-        first_fcst = pd.Timestamp( self.catalog_object.describe()['metadata']['forecast_limits']['start'] )#cftime.num2date(self.catalog_object.describe()['metadata']['forecast_limits']['start'], self.catalog_object.describe()['metadata']['forecast_limits']['units'], calendar=self.catalog_object.describe()['metadata']['forecast_limits']['calendar'])
-        last_fcst = pd.Timestamp( self.catalog_object.describe()['metadata']['forecast_limits']['end'] ) if type ( self.catalog_object.describe()['metadata']['forecast_limits']['end'] ) == str else pd.Timestamp.today() #cftime.num2date(self.catalog_object.describe()['metadata']['forecast_limits']['end'], self.catalog_object.describe()['metadata']['forecast_limits']['units'], calendar=self.catalog_object.describe()['metadata']['forecast_limits']['calendar']) if self.catalog_object.describe()['metadata']['forecast_limits']['end'] != -1 else fdate
+        first_fcst = pd.Timestamp( self.catalog_object.describe()['metadata']['forecast_limits']['start'] ) 
+        last_fcst = pd.Timestamp( self.catalog_object.describe()['metadata']['forecast_limits']['end'] ) if type ( self.catalog_object.describe()['metadata']['forecast_limits']['end'] ) == str else pd.Timestamp.today()
 
+        assert fdate >= first_fcst, 'requested initialization of {} predates first forecast by {} which happened on {}'.format(fdate, self.catalog_object.configure_new().name, first_fcst )
+        assert fdate <= last_fcst, 'requested initialization of {} on {} does not yet exist'.format(self.catalog_object.configure_new().name, fdate )
 
-        first_year = first_year if first_year is not None else fdate.year
-        final_year = final_year if final_year is not None else fdate.year # try to use 'this year' if not specified
-
-        assert pd.Timestamp(first_year, fdate.month, 1) <= pd.Timestamp(last_fcst.year, last_fcst.month, 1), 'first_year ({} {}) must be earlier than or equal to the last available year ({} {})'.format(first_year, threeletters[fdate.month], last_fcst.year, threeletters[last_fcst.month])
-        if pd.Timestamp(first_year, fdate.month, 1) >= pd.Timestamp(first_fcst.year, fdate.month, 1):
-            print('first year "{}" not in range for {}: ({} {} - {} {}). Using LAST possible year!'.format(first_year, self.catalog_object.configure_new().name, first_fcst.year, threeletters[first_fcst.month], last_fcst.year, threeletters[last_fcst.month] ))
-            first_year =  last_fcst.year
-        assert pd.Timestamp(final_year, fdate.month, 1) >= pd.Timestamp(first_fcst.year, first_fcst.month, 1), 'final_year ({} {}) must be later than or equal to the last available year ({} {})'.format(final_year, threeletters[fdate.month], first_fcst.year, threeletters[first_fcst.month])
-        if pd.Timestamp(final_year, fdate.month, 1) >= pd.Timestamp(last_fcst.year, fdate.month, 1):
-            print('final year "{}" not in range for {}: ({} {} - {} {}). Using last possible year!'.format(final_year, self.catalog_object.configure_new().name, first_fcst.year, threeletters[first_fcst.month], last_fcst.year, threeletters[last_fcst.month] ))
-            final_year = last_fcst.year
-
-        final_year = min(final_year, last_fcst.year) if final_year is not None else fdate.year
-        first_year = max(first_year, first_fcst.year) if first_year is not None else fdate.year
         fdate, target, lead_low, lead_high = seasonal_target(fdate, target, lead_low, lead_high)
         url = eval('f"{}"'.format(self.forecast_url)) 
         use_dlauth = str(self.catalog_object.describe()['metadata']['dlauth_required'])
