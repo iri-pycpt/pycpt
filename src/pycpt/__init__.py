0809ef83386b28c6de808552af49c27313838ea0
Aaron Kaplan <aaron@iri.columbia.edu>
2023-05-09 16:34:45 -0400

    add load_configuration

--- src/pycpt/__init__.py
+++ src/pycpt/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Kyle J. C. Hall (pycpt-help@iri.columbia.edu)"
 __license__ = "MIT"
-__version__ = "2.2.0"
+__version__ = "2.2.1"
 
 from .notebook import *
-from cptextras import save_configuration
+from cptextras import save_configuration, load_configuration
