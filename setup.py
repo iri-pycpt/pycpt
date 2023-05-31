8709e15f370f59bf288cb5ff277d708225cdc0be
Aaron Kaplan <aaron@iri.columbia.edu>
2023-05-31 16:12:24 -0400

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(Path(__file__ ).parent / 'README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name="cptdl",
-    version="1.1.0",
+    version="1.1.1",
     author="Kyle Hall",
     author_email="pycpt-help@iri.columbia.edu",
     description=(
