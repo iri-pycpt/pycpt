5249e828b815801d5b664ea9d6f73ea4bf79b51b
Aaron Kaplan <aaron@iri.columbia.edu>
2023-06-12 10:39:47 -0400

    Version 2.3.0

--- src/pycpt/__init__.py
+++ src/pycpt/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Kyle J. C. Hall (pycpt-help@iri.columbia.edu)"
 __license__ = "MIT"
-__version__ = "2.2.2"
+__version__ = "2.3.0"
 
 from .notebook import *
 from cptextras import save_configuration, load_configuration
