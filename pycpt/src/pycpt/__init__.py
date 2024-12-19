89d39e47c863ed51cc3209c884346efef434cb03
Aaron Kaplan <aaron@iri.columbia.edu>
2024-12-19 14:06:03 -0500

    pycpt 2.9.3. No change, but there will be a new lock file that updates cpt-io.

--- pycpt/src/pycpt/__init__.py
+++ pycpt/src/pycpt/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "IRI (pycpt-help@iri.columbia.edu)"
 __license__ = "MIT"
-__version__ = "2.9.2"
+__version__ = "2.9.3"
 
 from .notebook import *
 from cptextras import save_configuration, load_configuration
