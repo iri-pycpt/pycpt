b3c4b562e914ee45ac03ec040bfe87afd36e1e9d
Tebsun123 <israel.tebsun@gmail.com>
2025-08-29 12:09:38 +0300

    setup.py

---
+++ pycpt/src/cptdl/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cptdl",
+    version="0.1.0",
+    packages=find_packages(),  # automatically find submodules
+    install_requires=[],       # list dependencies here, e.g. ['numpy', 'pandas']
+    description="cptdl module from PyCPT",
+    author="IRI PyCPT",
+)
