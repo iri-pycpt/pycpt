1d2cd3b17c1bd944900b2724aa569552928ad315
Aaron Kaplan <aaron@iri.columbia.edu>
2025-09-05 12:03:17 -0400

    pycpt 2.10.3

--- pycpt/src/pycpt/__init__.py
+++ pycpt/src/pycpt/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "IRI (pycpt-help@iri.columbia.edu)"
 __license__ = "MIT"
-__version__ = "2.10.0"
+__version__ = "2.10.3"
 
 from .notebook import *
 from cptextras import save_configuration, load_configuration
